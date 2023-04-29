22bf1a84ed04690ac6e4db479d6de61721ca7480
kadafi abdou <kadafi.abdou@outlook.com>
2023-04-29 14:28:04 +0200

    delete table user unused

--- src/main/java/com/example/backcasaarchitecte/controller/UserController.java
+++ src/main/java/com/example/backcasaarchitecte/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.backcasaarchitecte.controller;
 
-import com.example.backcasaarchitecte.entity.User;
-import com.example.backcasaarchitecte.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
