da6d37b8c038015af5e0bcb9e55ad6769b9148f7
kadafi abdou <kadafi.abdou@outlook.com>
2023-05-29 17:01:08 +0200

    Configure pom.xml for oauth2

--- src/main/java/com/example/backcasaarchitecte/config/CorsConfig.java
+++ src/main/java/com/example/backcasaarchitecte/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:56617")
+                .allowedOrigins("http://localhost:4200", "http://localhost:63313")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
